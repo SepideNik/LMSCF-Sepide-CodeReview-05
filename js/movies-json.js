e2ce6e9f51cbebb0f7728a1dfe784980aee0ffda
Sepide <Sepide.niki@gmail.com>
2020-02-15 01:03:43 +0100

    finish

---
+++ js/movies-json.js
@@ -0,0 +1,97 @@
+let Movies = JSON.parse(`{
+    "Movie": [
+       {
+         "id": "1",
+         "name": "The Conjuring",
+         "image": "img/TheConjuring.jpg",
+         "Storyline": "In 1971, Carolyn and Roger Perron move their family into a dilapidated Rhode Island farm house and soon strange things start happening around it with escalating nightmarish terror. In desperation, Carolyn contacts the noted paranormal investigators, Ed and Lorraine Warren, to examine the house. What the Warrens discover is a whole area steeped in a satanic haunting that is now targeting the Perron family wherever they go. To stop this evil, the Warrens will have to call upon all their skills and spiritual strength to defeat this spectral menace at its source that threatens to destroy everyone involved.",
+         "Director": "James Wan",
+         "Writers": "Chad Hayes, Carey W. Hayes",
+         "Stars": "Patrick Wilson, Vera Farmiga, Ron Livingston",
+         "Rate": "7.5",
+         "Year": "2013",
+         "Genre": "Horror, Mystery, Thriller ",         
+         "Like": "25"
+
+       },
+       {
+         "id": "2",
+         "name": "World War Z",
+         "image": "img/WorldWarZ.jpg",
+         "Storyline": "Life for former United Nations investigator Gerry Lane and his family seems content. Suddenly, the world is plagued by a mysterious infection turning whole human populations into rampaging mindless zombies. After barely escaping the chaos, Lane is persuaded to go on a mission to investigate this disease. What follows is a perilous trek around the world where Lane must brave horrific dangers and long odds to find answers before human civilization falls.",
+         "Director": "Marc Forster",
+         "Writers": "Matthew Michael Carnahan, Drew Goddard",
+         "Stars": "Brad Pitt, Mireille Enos, Daniella Kertesz",
+         "Rate": "7.0",
+         "Year": "2013",
+         "Genre": "Action, Adventure, Horror",         
+         "Like": "40"
+
+       },
+       {
+         "id": "3",
+         "name": "28 Weeks Later",
+         "image": "img/28WeeksLater.jpg",
+         "Storyline": "28 Weeks Later picks up six months after the Rage Virus has decimated the city of London. The US Army has restored order and is repopulating the quarantined city, when a carrier of the Rage Virus enters London and unknowingly re-ignites the spread of the deadly infection and the nightmare begins... again.",
+         "Director": "Juan Carlos Fresnadillo",
+         "Writers": "Rowan Joffe, Juan Carlos Fresnadillo",
+         "Stars": "Jeremy Renner, Rose Byrne, Robert Carlyle",
+         "Rate": "7.1",
+         "Year": "2007",
+         "Genre": "Drama, Horror, Sci-Fi",         
+         "Like": "50"
+       },
+       {
+         "id": "4",
+         "name": "The Babadook",
+         "image": "img/TheBabadook.jpg",
+         "Storyline": "Amelia, who lost her husband in a car crash on the way to give birth to Samuel, their only child, struggles to cope with her fate as a single mom. Samuel's constant fear of monsters and violent reaction to overcome the fear doesn't help her cause either, which makes her friends become distant. When things can not get any worse, they read a strange book in their house about the 'Babadook' monster that hides in the dark areas of their house. Even Amelia seems to feel the effect of Babadook and desperately tries in vain to destroy the book. The nightmarish experiences the two encounter form the rest of the story.",
+         "Director": "Jennifer Kent",
+         "Writers": "Jennifer Kent",
+         "Stars": "Essie Davis, Noah Wiseman, Daniel Henshall",
+         "Rate": "6.8",
+         "Year": "2014",
+         "Genre": "Drama, Horror",         
+         "Like": "45"
+       },
+       {
+         "id": "5",
+         "name": "Inception",
+         "image": "img/Inception.jpg",
+         "Storyline": "Dom Cobb is a skilled thief, the absolute best in the dangerous art of extraction, stealing valuable secrets from deep within the subconscious during the dream state, when the mind is at its most vulnerable. Cobb's rare ability has made him a coveted player in this treacherous new world of corporate espionage, but it has also made him an international fugitive and cost him everything he has ever loved. Now Cobb is being offered a chance at redemption. One last job could give him his life back but only if he can accomplish the impossible, inception. Instead of the perfect heist, Cobb and his team of specialists have to pull off the reverse: their task is not to steal an idea, but to plant one. If they succeed, it could be the perfect crime. But no amount of careful planning or expertise can prepare the team for the dangerous enemy that seems to predict their every move. An enemy that only Cobb could have seen coming.",
+         "Director": "Christopher Nolan",
+         "Writers": "Christopher Nolan",
+         "Stars": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+         "Rate": "8.8",
+         "Year": "2010",
+         "Genre": "Action, Adventure, Sci-Fi",         
+         "Like": "60"
+       },
+       {
+         "id": "6",
+         "name": "Interstellar",
+         "image": "img/Interstellar.jpg",
+         "Storyline": "Earth's future has been riddled by disasters, famines, and droughts. There is only one way to ensure mankind's survival: Interstellar travel. A newly discovered wormhole in the far reaches of our solar system allows a team of astronauts to go where no man has gone before, a planet that may have the right environment to sustain human life.",
+         "Director": "Christopher Nolan",
+         "Writers": "Jonathan Nolan, Christopher Nolan",
+         "Stars": "Matthew McConaughey, Anne Hathaway, Jessica Chastain",
+         "Rate": "8.6",
+         "Year": "2014",
+         "Genre": "Adventure, Drama, Sci-Fi",         
+         "Like": "81"
+       },
+       {
+         "id": "7",
+         "name": "The Revenant",
+         "image": "img/TheRevenant.jpg",
+         "Storyline": "While exploring uncharted wilderness in 1823, legendary frontiersman Hugh Glass sustains injuries from a brutal bear attack. When his hunting team leaves him for dead, Glass must utilize his survival skills to find a way back home while avoiding natives on their own hunt. Grief-stricken and fueled by vengeance, Glass treks through the wintry terrain to track down John Fitzgerald, the former confidant who betrayed and abandoned him.",
+         "Director": "Alejandro G. Iñárritu",
+         "Writers": "Mark L. Smith , Alejandro G. Iñárritu",
+         "Stars": "Leonardo DiCaprio, Tom Hardy, Will Poulter",
+         "Rate": "8.5",
+         "Year": "2015",
+         "Genre": "Action, Adventure, Biography",         
+         "Like": "85"
+       }
+    ]
+ }`)
---
+++ js/myscript.js
@@ -0,0 +1,67 @@
+var obj = Movies.Movie; 
+$(document).ready(function () {       
+    CreateContainer(obj);    
+})
+function AddLike(id) {
+
+   var num= parseInt($("#"+id).text());
+    num += 1;
+    $("#"+id).html(num);
+    
+}
+function DoSort() {
+
+    var arr = [];
+    //get id and likes
+   $('.numLike').each(function(){
+        console.log($(this).attr("id"));
+        arr.push([$(this).attr("id"),$(this).text()]); 
+             
+   })
+   //sort by likes
+    arr.sort(function(a, b) {
+        return a[1] - b[1];
+    })
+    var index, entry;
+    var newArr= [];
+    for (index = 0; index < arr.length; ++index) {
+        entry = arr[index];
+         $.each(obj,function (i,item) {
+            if(entry[0]== item.id){
+                //keep last number
+                item.Like = entry[1];
+                newArr.push(item);
+            }
+        })
+
+        //Create container again
+        CreateContainer(newArr);
+    }
+}
+function CreateContainer(obj) {
+    $('#divcontainer').html("");
+    var divItems = '';
+    $.each(obj,function (i,item) {
+       divItems += `<div class="card mb-3 col-sm-12 col-lg-6 mr-5  ml-3 text-white divMovie">
+                        <div class="row no-gutters">
+                            <div class="col-4">
+                                 <img src="` + item.image + `" class="card-img" alt="Movie">
+                            </div>
+                            <div class="col-8 divInfo">
+                                <div class="card-body">
+                                    <h5 class="card-title">` + item.name + `</h5>
+                                    <p class="card-text">` + item.Genre + `</p>                                    
+                                    <div class="divLike">
+                                        <p class="card-text pYear"><small class="text-muted">` + item.Year + `</small></p>
+                                        <i onclick="AddLike(` + item.id + `)" class="fa fa-thumbs-up">Like</i>
+                                        <p id="` + item.id + `" class="numLike">` + item.Like + `</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+        
+    })
+    $('#divcontainer').append(divItems);
+    
+}
